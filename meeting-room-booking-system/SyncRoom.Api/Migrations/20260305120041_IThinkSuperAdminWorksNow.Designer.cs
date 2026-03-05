2160b9a5d28d897037c7871aee7d0fc8f25622f6
oftu-evan-andrew <107543784+oftu-evan-andrew@users.noreply.github.com>
2026-03-05 21:56:02 +0800

    fixed: should use correct method for creating roles now

--- meeting-room-booking-system/SyncRoom.Api/Migrations/20260305120041_IThinkSuperAdminWorksNow.Designer.cs
+++ meeting-room-booking-system/SyncRoom.Api/Migrations/20260305120041_IThinkSuperAdminWorksNow.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SyncRoom.Api.Data;
 
@@ -10,9 +11,11 @@ using SyncRoom.Api.Data;
 namespace SyncRoom.Api.Migrations
 {
     [DbContext(typeof(SyncRoomContext))]
-    partial class SyncRoomContextModelSnapshot : ModelSnapshot
+    [Migration("20260305120041_IThinkSuperAdminWorksNow")]
+    partial class IThinkSuperAdminWorksNow
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ meeting-room-booking-system/SyncRoom.Api/Migrations/20260305120041_IThinkSuperAdminWorksNow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SyncRoom.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IThinkSuperAdminWorksNow : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { new Guid("5807cd12-36ff-45c7-99ee-89cef9e7657c"), "2fc0aeb3-8ce2-4a66-ad39-7c05203361f5", "SuperAdmin", "SUPERADMIN" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("5807cd12-36ff-45c7-99ee-89cef9e7657c"));
+        }
+    }
+}
